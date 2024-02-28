8ed5245c6883edee049e18bf8de1deb400b367ae
harshanand124 <149586547+harshanand124@users.noreply.github.com>
2024-02-28 23:31:04 +0530

    Update Q1.c

--- Q1.c
+++ Q1.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 int main() {
- int n; //size of array
+ int n; 
  printf("Enter the total number of elements : ");
  scanf("%d",&n);
- int arr[n]; //array declaration
- int key ; // example key element to search for
- int found = 0; // flag for whether key element is found
- int comparisons = 0; // counter for number of comparisons made
+ int arr[n]; 
+ int key ; r
+ int found = 0; 
+ int comparisons = 0; 
  printf("Enter the elements of array : ");
  for(int i = 0; i < n; i++)
  {
- scanf("%d",&arr[i]); // input array elements
+ scanf("%d",&arr[i]); 
  }
  printf("Enter the key : ");
  scanf("%d",&key);
 
- // linear search algorithm
+
 
  for (int i = 0; i < n; i++) {
- comparisons++; // increment comparison counter
+ comparisons++;
  if (arr[i] == key) {
- found = 1; // set flag to indicate key element is found
- break; // exit loop
+ found = 1;
+ break;
  }
  }
 
- // output results
+
  if (found) {
  printf("Key element found\n");
  } else {
