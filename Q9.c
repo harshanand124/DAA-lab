b1e05a2cab41262c478d7e1038b5e85faedada67
harshanand124 <149586547+harshanand124@users.noreply.github.com>
2024-04-03 23:16:44 +0530

    Update Q9.c

--- Q9.c
+++ Q9.c
@@ -1,78 +1,72 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+void merge(int arr[], int l, int m, int r) {
+ int i, j, k;
+ int n1 = m - l + 1;
+ int n2 = r - m;
+ int* L = (int*)malloc(sizeof(int) * n1);
+ int* R = (int*)malloc(sizeof(int) * n2);
+ for (i = 0; i < n1; i++)
+ L[i] = arr[l + i];
+ for (j = 0; j < n2; j++)
+ R[j] = arr[m + 1 + j];
+ i = 0;
+ j = 0;
+ k = l;
+ while (i < n1 && j < n2) {
+ if (L[i] <= R[j]) {
+ arr[k] = L[i];
+ i++;
+ }
+ else {
+ arr[k] = R[j];
+ j++;
+ }
+ k++;
+ }
+ while (i < n1) {
+ arr[k] = L[i];
+ i++;
+ k++;
+ }
+ while (j < n2) {
+ arr[k] = R[j];
+ j++;
+ k++;
+ }
 
-
-
-void merge(int arr[], int l, int mid, int r,int *comparison,int*inversion) {
-    int i = l;
-    int j = mid + 1;
-    int k = 0;
-    int q[r - l + 1];
-    
-
-    while (i <= mid && j <= r) {
-        (*comparison);  
-
-        if (arr[i] <= arr[j]) {
-            q[k] = arr[i];
-            i++;
-        } else {
-            q[k] = arr[j];
-            j++;
-            
-        }
-        k++;
-    }
-
-    while (i <= mid) {
-        q[k] = arr[i];
-        i++;
-        k++;
-    }
-
-    while (j <= r) {
-        q[k] = arr[j];
-        j++;
-        k++;
-    }
-
-    for (k = 0, i = l; i <= r; i++, k++) {
-        arr[i] = q[k];
-    }
 }
-
-void mergesort(int arr[], int l, int r) {
-    if (l >= r) {
-        return;
-    }
-    int mid = (l + r) / 2;
-    mergesort(arr, l, mid,comparison,inversion);
-    mergesort(arr, mid + 1, r,comparison,inversion);
-    merge(arr, l, mid, r,comparison,inversion);
+void mergeSort(int arr[], int l, int r) {
+ if (l < r) {
+ int m = l + (r - l) / 2;
+ mergeSort(arr, l, m);
+ mergeSort(arr, m + 1, r);
+ merge(arr, l, m, r);
+ }
 }
-
+int hasDuplicates(int arr[], int n) {
+ mergeSort(arr, 0, n - 1);
+ for (int i = 0; i < n - 1; i++) {
+ if (arr[i] == arr[i + 1]) {
+ return 1;
+ }
+ }
+ return 0;
+}
 int main() {
-    int n;
-    printf("Enter the value of n: ");
-    scanf("%d", &n);
-    int arr[n];
-    for (int i = 0; i < n; i++) {
-        printf("Enter element %d: ", i + 1);
-        scanf("%d", &arr[i]);
-    }
-    int l = 0;
-    int r = n - 1;
-   int inversioncount = 0; 
-   int comparisoncount = 0;  
-    mergesort(arr, l, r,&comparisoncount,&inversioncount);
-
-    printf("Sorted array: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-
-    printf("\nNumber of comparisons: %d\n", comparisoncount);
-    printf("Number of inversions: %d\n", inversioncount);
-
-    return 0;
-}
+ int n;
+ printf("Enter the total number of elements : ");
+ scanf("%d",&n);
+ int arr[n];
+ printf("Enter the elements of the array : ");
+ for( int t = 0; t < n; t++)
+ {
+ scanf("%d",&arr[t]);
+ }
+ if (hasDuplicates(arr, n)) {
+ printf("YES\n");
+ }
+ else {
+ printf("NO\n");
+ }
+ return 0
